68e5c2d2ed1e9704a3cd1273d8f0bda2f47044d0
MaciejKrzysiak <krzysiakmaciej159@gmail.com>
2021-04-09 20:07:13 -0500

    Bing Search Through Editor Context Menu

--- extension.js
+++ extension.js
@@ -1,7 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const { default: axios } = require('axios');
+const editor = vscode.window.activeTextEditor;
+const empty_query = "Failed to search an empty query."
 
 async function showInputBox() {
 	const query = await vscode.window.showInputBox({
@@ -12,33 +11,33 @@ async function showInputBox() {
 	return query;
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "bing-search" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('bing-search.bingSearch', async function () {
-		// The code you place here will be executed every time your command is executed
+	let bingSearchDisposable = vscode.commands.registerCommand('bing-search.bingSearch', async function () {
 		query = await showInputBox()
-		vscode.env.openExternal("https://www.bing.com/search?q=" + query)
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Bing Search!');
+		if (query != undefined && query != null && query.trim() != "") {
+			vscode.env.openExternal("https://www.bing.com/search?q=" + query);
+		} else {
+			vscode.window.showInformationMessage(empty_query);
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	let editorContextMenuBingSearchDisposable = vscode.commands.registerCommand('bing-search.editorContextMenuBingSearch', async function () {
+		query = editor.document.getText(editor.selection);
+		if (query != undefined && query != null && query.trim() != "") {
+			vscode.env.openExternal("https://www.bing.com/search?q=" + query);
+		} else {
+			vscode.window.showInformationMessage(empty_query);
+		}
+	});
+  
+	context.subscriptions.push(bingSearchDisposable);
+	context.subscriptions.push(editorContextMenuBingSearchDisposable);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
