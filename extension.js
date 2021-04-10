e9047d62d620730fb6dfdcdec7d00f9c5fbf137f
MaciejKrzysiak <krzysiakmaciej159@gmail.com>
2021-04-09 22:42:30 -0500

    Fix Extension Activation Bug

--- extension.js
+++ extension.js
@@ -16,13 +16,13 @@ async function showInputBox() {
  */
 function activate(context) {
 
-	vscode.commands.executeCommand('setContext', 'bingSearch.viewCommandPaletteBingSearchInCommandPalette', true);
-	vscode.commands.executeCommand('setContext', 'bingSearch.viewEditorContextMenuBingSearchInCommandPalette', false);
+	vscode.commands.executeCommand('setContext', 'bing-search.viewCommandPaletteBingSearchInCommandPalette', true);
+	vscode.commands.executeCommand('setContext', 'bing-search.viewEditorContextMenuBingSearchInCommandPalette', false);
 
 	let bingSearchDisposable = vscode.commands.registerCommand('bing-search.commandPaletteBingSearch', async function () {
 		var query = await showInputBox()
 		if (query != undefined && query != null && query.trim() != "") {
-			vscode.env.openExternal("https://www.bing.com/search?q=" + query);
+			vscode.env.openExternal("https://www.bing.com/search?q=" + query.trim());
 		} else {
 			vscode.window.showInformationMessage(empty_query);
 		}
@@ -31,7 +31,7 @@ function activate(context) {
 	let editorContextMenuBingSearchDisposable = vscode.commands.registerCommand('bing-search.editorContextMenuBingSearch', async function () {
 		var query = editor.document.getText(editor.selection);
 		if (query != undefined && query != null && query.trim() != "") {
-			vscode.env.openExternal("https://www.bing.com/search?q=" + query);
+			vscode.env.openExternal("https://www.bing.com/search?q=" + query.trim());
 		} else {
 			vscode.window.showInformationMessage(empty_query);
 		}
