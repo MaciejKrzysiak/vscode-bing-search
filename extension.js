200f34c2affcc3ff1063939c7f72c238f801aca3
MaciejKrzysiak <krzysiakmaciej159@gmail.com>
2021-04-12 22:38:21 -0500

    Add Cntrl-B Shortcut Command

--- extension.js
+++ extension.js
@@ -11,6 +11,14 @@ async function showInputBox() {
 	return query;
 }
 
+async function queryBing(query) {
+	if (query != undefined && query != null && query.trim() != "") {
+		vscode.env.openExternal("https://www.bing.com/search?q=" + query.trim());
+	} else {
+		vscode.window.showInformationMessage(empty_query);
+	}
+}
+
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -18,27 +26,31 @@ function activate(context) {
 
 	vscode.commands.executeCommand('setContext', 'bing-search.viewCommandPaletteBingSearchInCommandPalette', true);
 	vscode.commands.executeCommand('setContext', 'bing-search.viewEditorContextMenuBingSearchInCommandPalette', false);
+	vscode.commands.executeCommand('setContext', 'bing-search.viewkeyBindedDecision', true);
 
 	let bingSearchDisposable = vscode.commands.registerCommand('bing-search.commandPaletteBingSearch', async function () {
 		var query = await showInputBox()
-		if (query != undefined && query != null && query.trim() != "") {
-			vscode.env.openExternal("https://www.bing.com/search?q=" + query.trim());
-		} else {
-			vscode.window.showInformationMessage(empty_query);
-		}
+		queryBing(query)
 	});
 
 	let editorContextMenuBingSearchDisposable = vscode.commands.registerCommand('bing-search.editorContextMenuBingSearch', async function () {
 		var query = editor.document.getText(editor.selection);
+		queryBing(query)
+	});
+
+	let keyBindedDecisionDisposable = vscode.commands.registerCommand('bing-search.keyBindedDecision', async function () {
+		query = editor.document.getText(editor.selection);
 		if (query != undefined && query != null && query.trim() != "") {
-			vscode.env.openExternal("https://www.bing.com/search?q=" + query.trim());
+			queryBing(query)
 		} else {
-			vscode.window.showInformationMessage(empty_query);
+			var query = await showInputBox()
+			queryBing(query)
 		}
 	});
   
 	context.subscriptions.push(bingSearchDisposable);
 	context.subscriptions.push(editorContextMenuBingSearchDisposable);
+	context.subscriptions.push(keyBindedDecisionDisposable)
 }
 
 function deactivate() {}
